ed9a96f58e747fd2a87d510903f0323038504771
VIRTUALGUARD <virtualguard101@gmail.com>
2025-09-07 23:14:05 +0800

    lab: update lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,18 +1,23 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author virtualguard101@gmail.com
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** 
+     * 
+    */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        int max = 0;
+        while (n > 1){
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n*3 + 1;
+            }
+            if (max < n) { max = n; }
         }
-    }
+        return max;
+   }
 
     public static void main(String[] args) {
         int n = 5;
